26dff5c89dad2c397834b43622d0fdd671a874a8
William Marrujo <willmarrujo@gmail.com>
2026-04-18 17:45:35 -0500

    add supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { supabase } from '$lib/supabaseClient'
+  import type { AuthSession } from '@supabase/supabase-js'
+  import Account from '$lib/Account.svelte'
+  import Auth from '$lib/Auth.svelte'
+
+  let session = $state<AuthSession | null>(null)
+
+  onMount(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      session = data.session
+    })
+
+    supabase.auth.onAuthStateChange((_event, _session) => {
+      session = _session
+    })
+  })
+</script>
+
+<div class="container" style="padding: 50px 0 100px 0">
+  {#if !session}
+    <Auth />
+  {:else}
+    <Account {session} />
+  {/if}
+</div>
